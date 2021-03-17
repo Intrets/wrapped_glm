62beb53f86b4ca710d9e3e2b74fb48a676e217bd
Intrets <geen.inspiratie824@gmail.com>
2021-03-17 01:58:34 +0100

    compiling with Wall and Werror. Sadly needing to use a weird workaround to disable warnings from glm

---
+++ include/wglm/glm.hpp
@@ -0,0 +1,8 @@
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable: 4201)
+#endif
+#include <glm/glm.hpp>
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
---
+++ include/wglm/gtc/constants.hpp
@@ -0,0 +1,8 @@
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable: 4201)
+#endif
+#include <glm/gtc/constants.hpp>
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif

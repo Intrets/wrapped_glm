c8da24601d38514940dcfe53d030f8e59ee05bea
Intrets <geen.inspiratie824@gmail.com>
2021-06-11 16:51:14 +0200

    precompiled headers

--- include/wglm/glm.hpp
+++ include/wglm/glm.hpp
@@ -1,6 +1,6 @@
 #ifdef _MSC_VER
 #pragma warning(push)
-#pragma warning(disable: 4201; disable: 4324)
+#pragma warning(disable: 4201; disable: 4324; disable: 4310)
 #endif
 #include <glm/glm.hpp>
 #ifdef _MSC_VER
